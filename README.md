cd1c8f9c67027c6665a59fff876361c2526b162d
Matteo Marchesini <matteo.marchesini12@studio.unibo.it>
2019-09-25 17:37:39 +0200

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# SimulationSystems
-Project of Simulation Systems course for the master degree at University of Bologna @unibo
---
+++ TwoPhaseNetwork/Source.cc
@@ -0,0 +1,109 @@
+//
+// This file is part of an OMNeT++/OMNEST simulation example.
+//
+// Copyright (C) 2006-2015 OpenSim Ltd.
+//
+// This file is distributed WITHOUT ANY WARRANTY. See the file
+// `license' for details on this and other legal matters.
+//
+
+#include "Source.h"
+#include "Job.h"
+#include <iostream>
+
+namespace queueing {
+
+void SourceBase::initialize()
+{
+    createdSignal = registerSignal("created");
+    jobCounter = 0;
+    WATCH(jobCounter);
+}
+
+Job *SourceBase::createJob()
+{
+    char buf[80];
+    int priority = par("jobPriority").intValue();
+    std::string name = "";
+    int type = 0;
+    if (priority >0){
+            name = "U1";
+            type = 1;
+
+    }else{
+            name = "U2";
+            type = 0;
+    }
+    jobName = name;
+    sprintf(buf, "%.60s-%d", jobName.c_str(), ++jobCounter);
+    Job *job = new Job(buf);
+    job->setKind(type);
+    job->setPriority(priority);
+    std::cerr << job->getKind();
+    std::cerr << job->getPriority();
+    std::cerr << "\n";
+    return job;
+}
+
+void SourceBase::finish()
+{
+    emit(createdSignal, jobCounter);
+}
+
+//----
+
+Define_Module(Source);
+
+void Source::initialize()
+{
+    SourceBase::initialize();
+    startTime = par("startTime");
+    stopTime = par("stopTime");
+    numJobs = par("numJobs");
+
+    // schedule the first message timer for start time
+    scheduleAt(startTime, new cMessage("newJobTimer"));
+}
+
+void Source::handleMessage(cMessage *msg)
+{
+    ASSERT(msg->isSelfMessage());
+
+    if ((numJobs < 0 || numJobs > jobCounter) && (stopTime < 0 || stopTime > simTime())) {
+        // reschedule the timer for the next message
+        scheduleAt(simTime() + par("interArrivalTime").doubleValue(), msg);
+
+        Job *job = createJob();
+        send(job, "out");
+    }
+    else {
+        // finished
+        delete msg;
+    }
+}
+
+//----
+
+Define_Module(SourceOnce);
+
+void SourceOnce::initialize()
+{
+    SourceBase::initialize();
+    simtime_t time = par("time");
+    scheduleAt(time, new cMessage("newJobTimer"));
+}
+
+void SourceOnce::handleMessage(cMessage *msg)
+{
+    ASSERT(msg->isSelfMessage());
+    delete msg;
+
+    int n = par("numJobs");
+    for (int i = 0; i < n; i++) {
+        Job *job = createJob();
+        send(job, "out");
+    }
+}
+
+}; //namespace
+
